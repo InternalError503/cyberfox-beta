f6488d6ad0a54dedccee781f4ba6c3ed6f92c6eb
Toady <support@8pecxstudios.com>
2015-01-01 01:44:24 +1000

    Refactor hiding entries for cyberfox features in to single function

--- browser/base/content/browser-cyberfox.js
+++ browser/base/content/browser-cyberfox.js
@@ -15,18 +15,18 @@ var gCyberfoxCustom = {
 
 	restartBrowser: function (e) {	
 			//Added in some general error handling by encapsulating it in try catch statements
-try{
-	//Minified its to fall in-line with restart my fox
-	if (Services.prefs.getBoolPref("browser.restart.purgecache")){
-		
-			Services.appinfo.invalidateCachesOnRestart();
-					Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup).quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);		
-			}else{
-					Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup).quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);;
-	}
-			}catch (e){
-				//Catch any nasty errors and output to console
-				console.log("Were sorry but something has gone wrong with 'restartBrowser' " + e);					
+	try{
+		//Minified its to fall in-line with restart my fox
+		if (Services.prefs.getBoolPref("browser.restart.purgecache")){
+			
+				Services.appinfo.invalidateCachesOnRestart();
+						Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup).quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);		
+				}else{
+						Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup).quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);;
+		}
+		}catch (e){
+			//Catch any nasty errors and output to console
+			console.log("Were sorry but something has gone wrong with 'restartBrowser' " + e);					
 		}
         
 },
@@ -50,51 +50,56 @@ try{
 	//Copies current tab url to clipboard	
 	CopyCurrentTabUrl : function(e) {
 
-try{	
-	
-	const gClipboardHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
-                                   .getService(Components.interfaces.nsIClipboardHelper);
-	gClipboardHelper.copyString(content.location.href);
+	try{	
 		
+			const gClipboardHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+										   .getService(Components.interfaces.nsIClipboardHelper);
+			gClipboardHelper.copyString(content.location.href);
+			
 		}catch (e){
 			//Catch any nasty errors and output to console
 			console.log("Were sorry but something has gone wrong with 'CopyCurrentTabUrl' " + e);
-	}			
+		}			
 },
 	
-isEnabled: function(element){
-	document.getElementById(element).hidden = false;
-},	
+	_ElementState :  function (element, state){
 	
-isDisabled: function(element){
-	document.getElementById(element).hidden = true;
-},	
+	try{	
+	
+			if (!typeof true || !typeof false){return;}
+			document.getElementById(element).hidden = state;
+			
+		}catch (e){
+				//Catch any nasty errors and output to console
+				console.log("Were sorry but something has gone wrong with '_ElementState' " + e);
+		}			 
+  },
 
 customPrefSettings: function(e){
 		
 document.getElementById("tabContextMenu")
         .addEventListener("popupshowing", function (e) {
 		
-try{
+	try{
 						
-	//C.L.O.N.E
-	if (Services.prefs.getBoolPref("browser.tabs.clonetab")){		
-			gCyberfoxCustom.isEnabled("context_CloneCurrentTab");
-			gCyberfoxCustom.isEnabled("context_CloneCurrentTabNewWindow");			
-	}else{
-			gCyberfoxCustom.isDisabled("context_CloneCurrentTab");
-			gCyberfoxCustom.isDisabled("context_CloneCurrentTabNewWindow");
-	}
-	//Copy tab url
-	if (Services.prefs.getBoolPref("browser.tabs.copyurl")){
-			gCyberfoxCustom.isEnabled("context_CopyCurrentTabUrl");	
-	}else{
-			gCyberfoxCustom.isDisabled("context_CopyCurrentTabUrl");
-	}
+		//C.L.O.N.E
+		if (Services.prefs.getBoolPref("browser.tabs.clonetab")){		
+				gCyberfoxCustom._ElementState("context_CloneCurrentTab", false);
+				gCyberfoxCustom._ElementState("context_CloneCurrentTabNewWindow", false);	
+		}else{
+				gCyberfoxCustom._ElementState("context_CloneCurrentTab", true);
+				gCyberfoxCustom._ElementState("context_CloneCurrentTabNewWindow", true);
+		}
+		//Copy tab url
+		if (Services.prefs.getBoolPref("browser.tabs.copyurl")){
+				gCyberfoxCustom._ElementState("context_CopyCurrentTabUrl", false);
+		}else{
+				gCyberfoxCustom._ElementState("context_CopyCurrentTabUrl", true);
+		}
 	
-		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'browser.tabs.clonetab' | browser.tabs.copyurl " + e);
+	}catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'browser.tabs.clonetab' | browser.tabs.copyurl " + e);
 	}
 	
   
@@ -103,18 +108,18 @@ try{
 document.getElementById("contentAreaContextMenu")
         .addEventListener("popupshowing", function (e) {
 		
-try{
+	try{
 						
-	//Email link
-	if (Services.prefs.getBoolPref("browser.context.emaillink")){		
-			gCyberfoxCustom.isEnabled("context-sendLink");		
-	}else{
-			gCyberfoxCustom.isDisabled("context-sendLink");
-	}
-	
-		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'browser.context.emaillink' " + e);
+		//Email link
+		if (Services.prefs.getBoolPref("browser.context.emaillink")){		
+				gCyberfoxCustom._ElementState("context-sendLink", false);
+		}else{
+				gCyberfoxCustom._ElementState("context-sendLink", true);
+		}
+		
+	}catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'browser.context.emaillink' " + e);
 	}
 	
   
@@ -124,18 +129,18 @@ try{
 document.getElementById("menu_ToolsPopup")
         .addEventListener("popupshowing", function (e) {
 
-try{		
+	try{		
 			
-	//R.A.M
-	if (Services.prefs.getBoolPref("clean.ram.cache")){
-			gCyberfoxCustom.isEnabled("minimizeMemoryUsage");
-	}else{
-			gCyberfoxCustom.isDisabled("minimizeMemoryUsage");
-    }
+		//R.A.M
+		if (Services.prefs.getBoolPref("clean.ram.cache")){
+				gCyberfoxCustom._ElementState("minimizeMemoryUsage", false);
+		}else{
+				gCyberfoxCustom._ElementState("minimizeMemoryUsage", true);
+		}
 	
-		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'clean.ram.cache' " + e);
+	}catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'clean.ram.cache' " + e);
 	}	
 			
   }, false);
@@ -143,18 +148,18 @@ try{
 document.getElementById("menu_FilePopup")
         .addEventListener("popupshowing", function (e) {
 
-try{		
+	try{		
 			
-	//R.M.F	
-	if (Services.prefs.getBoolPref("browser.restart.enabled")){ 
-			gCyberfoxCustom.isEnabled("app_restartBrowser");
-	}else{
-			gCyberfoxCustom.isDisabled("app_restartBrowser");
-	}
+		//R.M.F	
+		if (Services.prefs.getBoolPref("browser.restart.enabled")){ 
+				gCyberfoxCustom._ElementState("app_restartBrowser", false);
+		}else{
+				gCyberfoxCustom._ElementState("app_restartBrowser", true);
+		}
 	
-		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'browser.restart.enabled' " + e);
+	}catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'browser.restart.enabled' " + e);
 	}	
 			
   }, false);  
@@ -162,18 +167,18 @@ try{
  document.getElementById("toolbar-context-menu")
         .addEventListener("popupshowing", function (e) {
 
-try{		
+	try{		
 			
-	//About:config
-	if (Services.prefs.getBoolPref("browser.context.aboutconfig")){ 
-			gCyberfoxCustom.isEnabled("menu-aboutconfig");
-	}else{
-			gCyberfoxCustom.isDisabled("menu-aboutconfig");
-	}
+		//About:config
+		if (Services.prefs.getBoolPref("browser.context.aboutconfig")){ 
+				gCyberfoxCustom._ElementState("menu-aboutconfig", false);
+		}else{
+				gCyberfoxCustom._ElementState("menu-aboutconfig", true);
+		}
 	
-		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'browser.context.aboutconfig' " + e);
+	}catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'browser.context.aboutconfig' " + e);
 	}	
 			
   }, false); 
@@ -181,18 +186,18 @@ try{
    document.getElementById("menu_viewPopup")
         .addEventListener("popupshowing", function (e) {
 
-try{		
+	try{		
 			
-	//About:config
-	if (Services.prefs.getBoolPref("browser.menu.aboutconfig")){ 
-			gCyberfoxCustom.isEnabled("AboutConfigMenuItem");
-	}else{
-			gCyberfoxCustom.isDisabled("AboutConfigMenuItem");
-	}
+		//About:config
+		if (Services.prefs.getBoolPref("browser.menu.aboutconfig")){ 
+				gCyberfoxCustom._ElementState("AboutConfigMenuItem", false);
+		}else{
+				gCyberfoxCustom._ElementState("AboutConfigMenuItem", true);
+		}
 	
-		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'browser.menu.aboutconfig' " + e);
+	}catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'browser.menu.aboutconfig' " + e);
 	}	
 			
   }, false); 
@@ -202,42 +207,42 @@ try{
 
 	toolsDownloads: function(e){	
 	
-try{		
+	try{		
 
-	if (Services.prefs.getBoolPref("browser.download.useToolkitUI")) {	
-		if(DownloadsWindow == null || DownloadsWindow.closed){
-					window.open("chrome://browser/content/downloadUI.xul", "Downloads", params);					
-				}else{
-						DownloadsWindow.focus();
-					}	
-	  }else{
-		    BrowserDownloadsUI();	  
-	  }	  
-	 		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'toolsDownloads' " + e);
+		if (Services.prefs.getBoolPref("browser.download.useToolkitUI")) {	
+			if(DownloadsWindow == null || DownloadsWindow.closed){
+						window.open("chrome://browser/content/downloadUI.xul", "Downloads", params);					
+					}else{
+							DownloadsWindow.focus();
+						}	
+		  }else{
+				BrowserDownloadsUI();	  
+		  }	  
+	 }catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'toolsDownloads' " + e);
 	} 
 },
 
 customDownloadsOverlay: function(e){
 
-try{		
+	try{		
 	
-	if (Services.prefs.getBoolPref("browser.download.useToolkitUI")) {		
-				 	
-			if(DownloadsWindow == null || DownloadsWindow.closed){
-					window.open("chrome://browser/content/downloadUI.xul", "Downloads", params);
-				}else{
-						DownloadsWindow.focus();
-					}					
-	  }else{
-		        DownloadsPanel.showDownloadsHistory();  
-	  }	
+		if (Services.prefs.getBoolPref("browser.download.useToolkitUI")) {		
+						
+				if(DownloadsWindow == null || DownloadsWindow.closed){
+						window.open("chrome://browser/content/downloadUI.xul", "Downloads", params);
+					}else{
+							DownloadsWindow.focus();
+						}					
+		  }else{
+					DownloadsPanel.showDownloadsHistory();  
+		  }	
 
 	
-		}catch (e){
-			//Catch any nasty errors and output to console
-			console.log("Were sorry but something has gone wrong with 'customDownloadsOverlay' " + e);
+	}catch (e){
+		//Catch any nasty errors and output to console
+		console.log("Were sorry but something has gone wrong with 'customDownloadsOverlay' " + e);
 	}	
 
 }	
