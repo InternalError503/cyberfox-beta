b31e40ac5ef1d43847df71a46b1fcab2f830474f
toady <support@8pecxstudios.com>
2015-05-04 13:17:06 +1000

    Don't Record Shutdown Start TimeStamp
    Don't generate "Telemetry.ShutdownTime.txt" during shutdown process.

--- toolkit/components/startup/nsAppStartup.cpp
+++ toolkit/components/startup/nsAppStartup.cpp
@@ -377,7 +377,6 @@ nsAppStartup::Quit(uint32_t aMode)
     }
 
     PROFILER_MARKER("Shutdown start");
-    mozilla::RecordShutdownStartTimeStamp();
     mShuttingDown = true;
     if (!mRestart) {
       mRestart = (aMode & eRestart) != 0;
