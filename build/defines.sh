00167229fd0ed5034b4b9d70068d7862a93393c6
toady <support@8pecxstudios.com>
2015-08-06 16:11:42 +1000

    Merge: cyberfox 40.0prc2 with cyberfox 40.0prc1

--- build/defines.sh
+++ build/defines.sh
@@ -1,12 +1,12 @@
 # Define indicating that this build is prior to one of the early betas. To be
 # unset mid-way through the beta cycle.
-EARLY_BETA_OR_EARLIER=1
+EARLY_BETA_OR_EARLIER=0
 
 # Set the browser branding identity color
 IDENTITY_BRANDING_AMD=
-IDENTITY_BRANDING_BETA=1
+IDENTITY_BRANDING_BETA=
 IDENTITY_BRANDING_LINUX=
-IDENTITY_BRANDING_INTEL=
+IDENTITY_BRANDING_INTEL=1
 
 # Enable Agents.json
 MOZ_AGENTS=1
