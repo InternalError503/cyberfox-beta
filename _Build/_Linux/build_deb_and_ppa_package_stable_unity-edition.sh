d45c9ac852ce34cc994df75e34894306a0b55ed8
hawkeye116477 <hawkeye116477@gmail.com>
2016-09-01 11:13:14 +0200

    Add templates for creating .deb packages and uploading Cyberfox to ppa

---
+++ _Build/_Linux/build_deb_and_ppa_package_stable_unity-edition.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Built from template by hawkeye116477
+# Full repo https://github.com/hawkeye116477/cyberfox-deb
+# Script Version: 1.0
+
+# Init vars
+VERSION=""
+
+# Get package version.
+if [ -f "../../../browser/config/version_display.txt" ]; then
+    VERSION=$(<../../../browser/config/version_display.txt)
+else
+    echo "Unable to get current build version!"
+    exit 1    
+fi
+
+# Create needed folders 
+Dir=$(cd "$(dirname "$0")" && pwd)
+cd $Dir/
+mkdir debs # Create folder where move created .deb packages
+mkdir deb_and_ppa_stable
+cd $Dir/deb_and_ppa_stable/
+mkdir cyberfox-unity-edition-$VERSION
+cd $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/
+mkdir debian
+mkdir usr
+cd $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/usr/
+mkdir share
+cd $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/usr/share/
+mkdir applications
+mkdir lintian
+cd $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/usr/
+mkdir lib
+cd $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/usr/lib/
+mkdir Cyberfox
+
+# Set current directory to directory of package.
+cd $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION
+
+# Copy PPA templates
+if [ -f "$Dir/deb_and_ppa_templates/stable_unity-edition/" ]; then
+	cp -avr $Dir/deb_and_ppa_templates/stable_unity-edition/ $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/debian/
+else
+    echo "Unable to locate deb & ppa templates!"
+    exit 1 
+fi
+
+# Copy template for copyright
+if [ -f "$Dir/deb_and_ppa_templates/copyright" ]; then
+    cp $Dir/deb_and_ppa_templates/copyright $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/debian
+else
+    echo "Unable to locate copyright template!"
+    exit 1 
+fi
+
+# Copy latest build
+if [ -d "../../../../obj64/dist/Cyberfox" ]; then
+    cp -R ../../../../obj64/dist/Cyberfox/* $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/usr/lib/Cyberfox
+	cp $Dir/deb_and_ppa_templates/cyberfox.desktop $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/usr/share/applications
+	cp $Dir/deb_and_ppa_templates/Cyberfox $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/usr/share/lintian/overrides
+    cp $Dir/deb_and_ppa_templates/Cyberfox.sh $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION
+	cp $Dir/deb_and_ppa_templates/vendor-gre.js $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION
+else
+    echo "Unable to Cyberfox package files, Please check the build was created and packaged successfully!"
+    exit 1     
+fi
+
+# Make sure correct permissions are set
+chmod  755 $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/debian/control
+chmod  755 $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/debian/prerm
+chmod  755 $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/debian/copyright
+
+# Make symlinks
+ln -s /usr/lib/Cyberfox/Cyberfox.sh $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/cyberfox
+ln -s /usr/lib/Cyberfox/browser/icons/mozicon128.png $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION/Cyberfox.png
+
+# Build .deb package
+debuild -us
+mv $Dir/deb_and_ppa_stable/cyberfox-unity-edition-$VERSION-0~ppa1_amd64.deb $Dir/debs/cyberfox-unity-edition-$VERSION.en-US.linux-x86_64.deb
+
+# Build package for upload to PPA
+debuild -S -sa
+
+# Upload package to cyberfox PPA
+cd $Dir/deb_and_ppa
+dput ppa:8pecxstudios/cyberfox cyberfox-unity-edition-$VERSION-0~ppa1.source.changes
+
+# Clean up
+if [ -f "$Dir/deb_and_ppa_stable" ]; then
+    echo "Clean: $Dir/deb_and_ppa_stable"
+    rm -rf $Dir/deb_and_ppa_stable
+fi
