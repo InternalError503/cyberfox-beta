a7cc51a3002282dccd8dae07bc45e601ae81a71c
hawkeye116477 <hawkeye116477@gmail.com>
2016-09-04 18:44:09 +0200

    Update of deb and ppa templates

--- _Build/_Linux/build_deb_and_ppa_package_stable.sh
+++ _Build/_Linux/build_deb_and_ppa_package_stable.sh
@@ -13,13 +13,13 @@ IDENTITY=$2
 
 # Check if source code url was passed to script
 if [ -z "$SOURCE" ] || [ ! -n "$SOURCE" ]; then
-    echo "Source url must be passed to the script build_deb_package.sh 'URL'"
+    echo "Source url must be passed to the script build_deb_and_ppa_package.sh 'URL'"
     exit 1
 fi
 
 # Check if IDENTITY was passed to script
 if [ -z "$IDENTITY" ] || [ ! -n "$IDENTITY" ]; then
-    echo "IDENTITY type must be passed to the script build_deb_package.sh 'IDENTITY'"
+    echo "IDENTITY type must be passed to the script build_deb_and_ppa_package.sh 'IDENTITY'"
     exit 1
 fi
 
@@ -63,13 +63,24 @@ fi
 # Set current directory to directory of package.
 cd $Dir/deb_ppa/cyberfox-$VERSION
 
-# Copy PPA templates
+# Copy DEB and PPA templates
 if [ -d "$Dir/deb_and_ppa_templates/_template" ]; then
 	cp -r $Dir/deb_and_ppa_templates/_template/* $Dir/deb_ppa/cyberfox-$VERSION/debian
 else
     echo "Unable to locate ppa templates!"
     exit 1 
 fi
+
+# Copy control file
+if [ -d "$Dir/deb_and_ppa_templates/" ]; then
+if [ "$IDENTITY" == "Release" ]; then
+	mv $Dir/deb_and_ppa_templates/control.release $Dir/deb_ppa/cyberfox-$VERSION/debian/control
+else if [ "$IDENTITY" == "Beta" ]; then
+	mv $Dir/deb_and_ppa_templates/control.beta $Dir/deb_ppa/cyberfox-$VERSION/debian/control
+else
+    echo "IDENTITY was not set. Unable to copy control file!"
+    exit 1 
+fi	
 
 # Generate change log template
 CHANGELOGDIR=$Dir/deb_ppa/cyberfox-$VERSION/debian/changelog
@@ -107,6 +118,7 @@ if [ -d "../../../../../obj64/dist/Cyberfox" ]; then
 	cp $Dir/deb_and_ppa_templates/Cyberfox $Dir/deb_ppa/cyberfox-$VERSION/usr/share/lintian/overrides
     cp $Dir/deb_and_ppa_templates/Cyberfox.sh $Dir/deb_ppa/cyberfox-$VERSION
 	cp $Dir/deb_and_ppa_templates/vendor-gre.js $Dir/deb_ppa/cyberfox-$VERSION
+	cp $Dir/deb_and_ppa_templates/vendor-cyberfox.js $Dir/deb_ppa/cyberfox-$VERSION
 else
     echo "Unable to Cyberfox package files, Please check the build was created and packaged successfully!"
     exit 1     
@@ -120,7 +132,8 @@ fi
 
 # Make sure correct permissions are set
 chmod  755 $Dir/deb_ppa/cyberfox-$VERSION/debian/control
-chmod  755 $Dir/deb_ppa/cyberfox-$VERSION/debian/prerm
+chmod  755 $Dir/deb_ppa/cyberfox-$VERSION/debian/cyberfox.prerm
+chmod  755 $Dir/deb_ppa/cyberfox-$VERSION/debian/cyberfox.postinst
 chmod  755 $Dir/deb_ppa/cyberfox-$VERSION/debian/copyright
 
 # Make symlinks
