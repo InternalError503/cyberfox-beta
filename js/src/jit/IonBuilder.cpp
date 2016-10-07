cfc8947d523347a193af600c6c4b463f75bb28bf
toady <support@8pecxstudios.com>
2016-10-07 15:37:40 +1000

    Merge: cyberfox 50.0b3 with cyberfox 50.0b2

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -14576,3 +14576,14 @@ IonBuilder::convertToBoolean(MDefinition* input)
 
     return result;
 }
+
+void
+IonBuilder::trace(JSTracer* trc)
+{
+    if (!compartment->runtime()->runtimeMatches(trc->runtime()))
+        return;
+
+    TraceManuallyBarrieredEdge(trc, &script_, "IonBuiler::script_");
+    if (actionableAbortScript_)
+        TraceManuallyBarrieredEdge(trc, &actionableAbortScript_, "IonBuiler::actionableAbortScript_");
+}
