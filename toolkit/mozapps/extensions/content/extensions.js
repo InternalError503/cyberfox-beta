a502290e860fae1e255c31bdf6d340dc686d3d7c
toady <support@8pecxstudios.com>
2016-12-04 12:24:47 +1000

    Added: back plugin check
    Highly requested feature to add back, This feature was removed from
    firefox as the plugin check did not account for plugin blocklist and
    would tell users there plugin was upto date and safe when not see
    https://bugzilla.mozilla.org/show_bug.cgi?id=1277905

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -875,6 +875,15 @@ var gViewController = {
       }
     },
 
+    cmd_pluginCheck: {
+      isEnabled: function() {
+        return true;
+      },
+      doCommand: function() {
+        openURL(Services.urlFormatter.formatURLPref("plugins.update.url"));
+      }
+    },
+
     cmd_toggleAutoUpdateDefault: {
       isEnabled: function() {
         return true;
